113076da0cb68f9a3461429ed5ab88ea00e2ebaf
Akmal <izuddin0407@gmail.com>
2022-10-24 16:34:22 +0800

    minor additions: learning the ropes with nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+import '../styles/globals.css';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        backgroundColor: '#000000'
+      }
+    }
+  },
+  colors: {
+    black: '#000000',
+    red: '#E53E3E'
+  }
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <ChakraProvider theme={ theme }>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  ); 
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import connectMongo from '../utils/connectMongo';
+import User from '../models/userModel';
 
-export default function Home() {
+import { Box } from "@chakra-ui/react";
+import { normalizeRepeatedSlashes } from 'next/dist/shared/lib/utils';
+
+export default function Home({ users }) {
   return(
-    <div>
-      Hello World
-    </div>
+    <Box display='grid' gridTemplateColumns='1fr 1fr 1fr'>
+      {
+        users.map(( user ) => {
+          return <Box key={ user._id } color='red' borderWidth='1px' borderRadius='lg' borderColor='red'>
+            { user.displayName }
+          </Box>
+        })
+      }
+    </Box>
   );
+};
+
+export async function getServerSideProps() {
+  try {
+    await connectMongo();
+
+    const users = await User.find();
+
+    return {
+      props: {
+        users: JSON.parse( JSON.stringify( users )),
+      }
+    };
+  }
+  catch( error ) {
+    console.log( error );
+    return {
+      notFound: true,
+    };
+  }
 };
