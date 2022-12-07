1d60bd1f74e1643f18b8bf5448382f9beb62b990
Akmal <izuddin0407@gmail.com>
2022-12-07 11:35:51 +0800

    minor changes => post form styling for desktop

--- components/navbar/PostForm.js
+++ components/navbar/PostForm.js
@@ -51,80 +51,96 @@ export const PostForm = ({ setFormOpen }) => {
   };
 
   return (
-    <Box
-      position={ 'fixed' }
-      height={ '100%' }
-      width={ '100%' }
-      backgroundColor={ 'gray.900' }
-      zIndex={ '1' }
-      top={ '0' }
-      right={ '0' }
+    <Flex
+      justifyContent={{ lg: 'center' }}
+      alignItems={{ lg: 'center' }}
+      position='fixed'
+      height='100%'
+      width='100%'
+      backgroundColor='rgba(0,0,0,0.7)'
+      zIndex='1'
+      top='0'
+      right='0'
     >
-      <Box
-        borderBottom={ '1px' }
-        borderColor={ 'red.500' }
-      >
-        <IconButton
-          icon={ <CloseIcon/> }
-          backgroundColor={ 'transparent' }
-          color={ 'red.500' }
-          onClick={ () => setFormOpen( false ) }
-        />
-      </Box>
+
       <Flex
-        direction={ 'row' }
-        padding={ '10px 20px' }
+        width={{ base: '100%', lg: '70%' }}
+        borderRadius={{ lg: '10px' }}
+        direction='column'
+        backgroundColor='gray.900'
       >
-        <Avatar
-          src={ session.user.image }
-          alt={ session.user.name }
-          size={ 'md' }
-        />
-        <Button
-          backgroundColor={ 'transparent' }
-          color={ 'red.500' }
+        <Box
+        borderBottom='1px'
+        borderColor='red.500'
         >
-          { session.user.name }
-        </Button>
+          <IconButton
+            icon={ <CloseIcon/> }
+            backgroundColor='transparent'
+            color='red.500'
+            onClick={ () => setFormOpen( false ) }
+          />
+        </Box>
+
+        <Flex
+          direction='row'
+          padding='10px 20px'
+        >
+          <Avatar
+            src={ session.user.image }
+            alt={ session.user.name }
+            size='md'
+          />
+          <Button
+            backgroundColor='transparent'
+            color='red.500'
+            _hover
+          >
+            { session.user.name }
+          </Button>
+        </Flex>
+
+        <FormControl
+          display='flex'
+          flexDirection='column'
+          alignItems='center'
+          padding='10px'
+        >
+          <Input
+            value={ imageContent }
+            onChange={(e) => setImageContent( e.target.value )}
+            type='url'
+            placeholder='Image URL...'
+            variant='filled'
+            color='red.500'
+            backgroundColor='transparent'
+            borderColor='red.500'
+            borderRadius='0'
+            marginBottom='10px'
+          />
+          <Textarea
+            value={ postContent }
+            onChange={ ( e ) => setPostContent( e.target.value ) }
+            placeholder={ `What's on your mind?` }
+            color='red.500'
+            resize='vertical'
+            borderColor='red.500'
+            borderRadius='0'
+          />
+          <Button
+            color='gray.900'
+            backgroundColor='red.500'
+            size='md'
+            padding='0 30px'
+            margin='10px'
+            onClick={ () => onPostCreate() }
+            alignSelf={{ lg: 'flex-end' }}
+            _hover
+          >
+            Post
+          </Button>
+        </FormControl>
       </Flex>
-      <FormControl
-        display={ 'flex' }
-        flexDirection={ 'column' }
-        alignItems={ 'center' }
-        padding={ '10px' }
-      >
-        <Input
-          value={ imageContent }
-          onChange={(e) => setImageContent( e.target.value )}
-          type='url'
-          placeholder='Image URL...'
-          variant='filled'
-          color='red.500'
-          backgroundColor='transparent'
-          borderColor='red.500'
-          borderRadius='0'
-          marginBottom='10px'
-        />
-        <Textarea
-          value={ postContent }
-          onChange={ ( e ) => setPostContent( e.target.value ) }
-          placeholder={ `What's on your mind?` }
-          color={ 'red.500' }
-          resize={ 'vertical' }
-          borderColor={ 'red.500' }
-          borderRadius={ '0' }
-        />
-        <Button
-          color={ 'gray.900' }
-          backgroundColor={ 'red.500' }
-          size={ 'md' }
-          padding={ '0 30px' }
-          margin={ '10px' }
-          onClick={ () => onPostCreate() }
-        >
-          Post
-        </Button>
-      </FormControl>
-    </Box>
+      
+    </Flex>
   );
 };
